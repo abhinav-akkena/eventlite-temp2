63b4069a8ccc87f45aa791b89a64ce1150a1c3fc
z72819ap <alan.prophett@student.manchester.ac.uk>
2024-02-15 16:51:09 +0000

    Implement events as a database entity
    Implement Event entity, Event service class, and load sample data.

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -10,6 +10,17 @@ import org.springframework.context.annotation.Profile;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+
+import java.util.ArrayList;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.springframework.core.io.ClassPathResource;
+
 
 @Configuration
 @Profile("default")
@@ -22,6 +33,8 @@ public class InitialDataLoader {
 
 	@Autowired
 	private VenueService venueService;
+	
+	private final static String DATA = "data/events.json";
 
 	@Bean
 	CommandLineRunner initDatabase() {
@@ -36,7 +49,13 @@ public class InitialDataLoader {
 				log.info("Database already populated with events. Skipping event initialization.");
 			} else {
 				// Build and save initial events here.
+				eventService.save(new Event(1, "COMP23412 Showcase 01", LocalDate.of(2024, 05, 07), LocalTime.of(9, 0),1)); 
+				eventService.save(new Event(2, "COMP23412 Showcase 02", LocalDate.of(2024, 05, 07), LocalTime.of(12, 0),1)); 
+				eventService.save(new Event(3, "COMP23412 Showcase 03", LocalDate.of(2024, 05, 07), LocalTime.of(15, 0),1)); 
 			}
+			
+		
+
 		};
 	}
 }
--- src/main/java/uk/ac/man/cs/eventlite/dao/EventRepository.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/EventRepository.java
@@ -1,12 +1,11 @@
 package uk.ac.man.cs.eventlite.dao;
+import org.springframework.data.repository.CrudRepository;
 
 import uk.ac.man.cs.eventlite.entities.Event;
 
-public interface EventService {
-
-	public long count();
+public interface EventRepository extends CrudRepository<Event, Long> {
 
 	public Iterable<Event> findAll();
-	
+
 	public Event save(Event e);
 }
