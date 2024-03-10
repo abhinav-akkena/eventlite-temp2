5471f1a60adbc4859b0a438fa148cb23d1f4dd17
z72819ap <alan.prophett@student.manchester.ac.uk>
2024-03-10 15:51:17 +0000

    Implement unit test for venue ordering
    Implement unit test to check venue ordered by name
    
    Ref: #24

--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApiIntegrationTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApiIntegrationTest.java
@@ -3,6 +3,14 @@ package uk.ac.man.cs.eventlite.controllers;
 import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +26,7 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -47,5 +56,16 @@ public class VenuesControllerApiIntegrationTest extends AbstractTransactionalJUn
 		client.get().uri("/venues/99").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isNotFound()
 				.expectHeader().contentType(MediaType.APPLICATION_JSON).expectBody().jsonPath("$.error")
 				.value(containsString("venue 99")).jsonPath("$.id").isEqualTo(99);
+	}
+	
+	@Test
+	public void getVenuesOrderedByName() throws Exception {
+		client.get().uri("/venues").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectHeader()
+			.contentType(MediaType.APPLICATION_JSON).expectBody()
+			.jsonPath("$._links.self.href").value(endsWith("/api/venues"))
+			.jsonPath("$._embedded.venues.length()").value(equalTo(3))
+			.jsonPath("$._embedded.venues[0].name").value(equalTo("AMBS Building"))
+			.jsonPath("$._embedded.venues[1].name").value(equalTo("Kilburn Building"))
+			.jsonPath("$._embedded.venues[2].name").value(equalTo("Online"));
 	}
 }
