3304c933c9390863f274b1bfb0d8035f73d9028f
z72819ap <alan.prophett@student.manchester.ac.uk>
2024-03-10 14:38:36 +0000

    Implemented get all venues with view and unit tests
    Implemented get all venues controller and user interface
    
    Ref: #24

--- src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
@@ -1,5 +1,53 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Venue;
+import uk.ac.man.cs.eventlite.exceptions.VenueNotFoundException;
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+
+@Controller
+@RequestMapping(value = "/venues", produces = { MediaType.TEXT_HTML_VALUE })
 public class VenuesController {
+
+	@Autowired
+	private VenueService venueServices;
+
+
+	@ExceptionHandler(VenueNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String eventNotFoundHandler(VenueNotFoundException ex, Model model) {
+		model.addAttribute("not_found_id", ex.getId());
+
+		return "venues/not_found";
+	}
+	
+	@GetMapping
+	public String getAllVenues(Model model) {
+		model.addAttribute("venues", venueServices.findAll());
+		return "venues/index";
+	}
 
 }
