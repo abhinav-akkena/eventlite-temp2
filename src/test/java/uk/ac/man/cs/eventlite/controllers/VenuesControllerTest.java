2bcb78906dd3d543d781e28d9227fac61c51cc7a
Vikramjeet Singh <vikramjeet.singh-2@student.manchester.ac.uk>
2024-04-10 23:19:26 +0100

    Add existing venue display test

--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -1,14 +1,17 @@
 package uk.ac.man.cs.eventlite.controllers;
 
 import static org.mockito.Mockito.verify;
+
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 import java.util.Collections;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,6 +29,7 @@ import uk.ac.man.cs.eventlite.config.Security;
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Venue;
+import uk.ac.man.cs.eventlite.entities.Event;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(VenuesController.class)
@@ -37,6 +41,11 @@ public class VenuesControllerTest {
 
 	@Mock
 	private Venue venue;
+	
+	@Mock
+	private Event event;
+	
+	
 
 	@MockBean
 	private VenueService venueService;
@@ -85,5 +94,31 @@ public class VenuesControllerTest {
 		mvc.perform(get("/venues/search").accept(MediaType.TEXT_HTML).param("inputSearch", "spaghettiMeatballs")).andExpect(status().isOk())
 			.andExpect(view().name("venues/index")).andExpect(handler().methodName("searchVenue"));
 	}
+	
+    @Test
+    public void showExistingVenue() throws Exception {
+        Long venueId = 1L;
+        when(venueService.findById(venueId)).thenReturn(venue);
+        when(eventService.findFuture()).thenReturn(Arrays.asList(event));
+        when(event.getVenue()).thenReturn(venue);
+        when(venue.getId()).thenReturn(venueId);
+
+        mvc.perform(get("/venues/{id}", venueId))
+                .andExpect(status().isOk())
+                .andExpect(view().name("venues/venueDetails"))
+                .andExpect(model().attributeExists("venue"))
+                .andExpect(model().attributeExists("events"))
+                .andExpect(handler().methodName("showVenueDetails"));
+
+        verify(venueService).findById(venueId);
+        verify(eventService).findFuture();
+    }
+	
+	
+	
+	
+	
+	
+	
 
 }
