27e02a918b25d31acd81f7958ed8f944ab70cd0b
z94717mc <matt.cook-6@student.manchester.ac.uk>
2024-04-23 10:06:52 +0100

    Write unit tests to provide coverage

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerTest.java
@@ -1,10 +1,15 @@
 package uk.ac.man.cs.eventlite.controllers;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*; 
 
 import static org.mockito.Mockito.verify;
 
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -21,6 +26,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.DefaultCsrfToken;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -31,6 +39,7 @@ import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
 import uk.ac.man.cs.eventlite.exceptions.EventNotFoundException;
+
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(EventsController.class)
@@ -52,6 +61,7 @@ public class EventsControllerTest {
 	@MockBean
 	private VenueService venueService;
 
+	private Optional<Event> testEvent;
 	@Test
 	public void getIndexWhenNoEvents() throws Exception {
 		when(eventService.findAll()).thenReturn(Collections.<Event>emptyList());
@@ -145,5 +155,55 @@ public class EventsControllerTest {
         verify(eventService).findById(invalidEventId);
     }
 	
+
+    @Test	
+    @WithMockUser(roles = {"ADMIN", "ADMINISTRATOR"})
+    public void deleteEventAsAdmin() throws Exception {
+	    long eventId = 1L;
+
+	    when(eventService.findById(eventId)).thenReturn(testEvent);
+
+	    CsrfToken csrfToken = new DefaultCsrfToken("X-CSRF-TOKEN", "_csrf", "token-value");
+	    
+	    mvc.perform(post("/events/delete/{id}", eventId)
+	        .sessionAttr("_csrf", csrfToken)
+	        .param("_csrf", csrfToken.getToken())
+	        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+	        ;
+		when(venueService.findAll()).thenReturn(Collections.<Venue>emptyList());
+
+    }
+    
+	@Test
+	public void searchEventsPast() throws Exception {
+	    Event pastEvent = new Event();
+	    LocalDate date = LocalDate.now();
+	    date = date.minusDays(1);
+	    pastEvent.setId(1L);
+	    pastEvent.setName("Test Event");
+	    pastEvent.setDate(date);
+
+
+		when(eventService.search("Test Event")).thenReturn(Collections.<Event>singletonList(event));
+		
+		mvc.perform(get("/events/search").accept(MediaType.TEXT_HTML).param("inputSearch", "Test Event")).andExpect(status().isOk())
+			.andExpect(view().name("events/index")).andExpect(handler().methodName("searchEvent"));
+	}
+	
+	@Test
+	public void searchEventsFuture() throws Exception {
+	    Event futureEvent = new Event();
+	    LocalDate date = LocalDate.now();
+	    date = date.plusDays(1);
+	    futureEvent.setId(1L);
+	    futureEvent.setName("Test Event");
+	    futureEvent.setDate(date);
+
+
+		when(eventService.search("Test Event")).thenReturn(Collections.<Event>singletonList(event));
+		
+		mvc.perform(get("/events/search").accept(MediaType.TEXT_HTML).param("inputSearch", "Test Event")).andExpect(status().isOk())
+			.andExpect(view().name("events/index")).andExpect(handler().methodName("searchEvent"));
+	}
 	
 }
